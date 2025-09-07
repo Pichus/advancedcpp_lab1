082a9546188c27eb01e5e763f9045a6d0c8f958a
pichus <dimatyzuk@gmail.com>
2025-09-07 16:26:30 +0300

    add and implement methods for deck generation and shuffle

--- src/DeckManager.cpp
+++ src/DeckManager.cpp
@@ -1,3 +1,33 @@
-#include "../DeckManager.h"
+#include "DeckManager.h"
+
+#include <algorithm>
+#include <random>
+
+DeckManager::DeckManager(const int cards_of_same_suit_count)
+    : current_card_index_(0),
+      cards_of_same_suit_count_(cards_of_same_suit_count) {
+  deck_size_ = kSuitCount * cards_of_same_suit_count_;
+  deck_.resize(deck_size_);
+  GenerateDeck();
+  ShuffleDeck();
+}
+
+void DeckManager::GenerateDeck() {
+  for (int i = 0; i < deck_size_; i++) {
+    const int card_suit = i / cards_of_same_suit_count_;
+    const int card_number = i % cards_of_same_suit_count_;
+    const Card card(card_suit, card_number);
+    deck_[i] = card;
+  }
+}
+
+void DeckManager::ShuffleDeck() {
+  current_card_index_ = 0;
+  
+  std::random_device random_device;
+  std::mt19937 mersenne_twister_engine(random_device());
+
+  std::ranges::shuffle(deck_, mersenne_twister_engine);
+}
 
 void DeckManager::operator()() const {}
--- src/DeckManager.h
+++ src/DeckManager.h
@@ -1,8 +1,23 @@
 #ifndef DECKMANAGER_H
 #define DECKMANAGER_H
+#include <vector>
+
+#include "Card.h"
 
 class DeckManager {
+ private:
+  static constexpr int kSuitCount = 4;
+
+  std::size_t current_card_index_;
+  int cards_of_same_suit_count_;
+  int deck_size_;
+  std::vector<Card> deck_;
+
+  void GenerateDeck();
+  void ShuffleDeck();
+
  public:
+  explicit DeckManager(int cards_of_same_suit_count);
   void operator()() const;
 };
 
