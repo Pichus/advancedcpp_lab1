ed61cacacfb9b4fffe6ad0500c8db04fb9fd3ef5
pichus <dimatyzuk@gmail.com>
2025-09-07 16:55:31 +0300

    add a parameterless constructor to card so it can be constructed inside a vector

--- src/Card.h
+++ src/Card.h
@@ -9,6 +9,7 @@ struct Card {
   int number_;
 
  public:
+  Card() : suit_(0), number_(0) {}
   explicit Card(const int suit, const int number)
       : suit_(suit), number_(number) {}
   auto operator<=>(const Card&) const = default;
